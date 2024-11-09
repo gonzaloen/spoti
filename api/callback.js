52fa20aa661cf2492fc0063dfe1558cc33e64f10
gonzaloen <34185901+gonzaloen@users.noreply.github.com>
2024-11-09 17:49:23 +0100

    Update callback.js

--- api/callback.js
+++ api/callback.js
@@ -1,6 +1,5 @@
 import axios from 'axios';
 import querystring from 'querystring';
-import cookie from 'cookie';
 
 export default async function handler(req, res) {
   try {
@@ -19,21 +18,9 @@ export default async function handler(req, res) {
     );
 
     const { access_token } = tokenResponse.data;
-
-    console.log('Token de acceso recibido en callback.js:', access_token); // Log para verificar el token
-
-    // Almacena el token de acceso en una cookie
-    res.setHeader('Set-Cookie', cookie.serialize('access_token', access_token, {
-      httpOnly: true,
-      secure: process.env.NODE_ENV === 'production',
-      maxAge: 3600, // Expira en una hora
-      path: '/',
-    }));
-
-    // Redirige al usuario a la página principal para ver los resultados
-    res.redirect('/');
+    res.redirect(`/api/recentlyPlayed?access_token=${access_token}`);
   } catch (error) {
-    console.error('Error en /api/callback:', error);
+    console.error(error);
     res.status(500).json({ error: 'Internal Server Error', details: error.message });
   }
 }
