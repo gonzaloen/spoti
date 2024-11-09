6f857b3f1a87409449317fdbbe557b2e1e58ae3b
gonzaloen <34185901+gonzaloen@users.noreply.github.com>
2024-11-09 17:14:34 +0100

    Update recentlyPlayed.js

--- api/recentlyPlayed.js
+++ api/recentlyPlayed.js
@@ -1,19 +1,24 @@
 import axios from 'axios';
 
 export default async function handler(req, res) {
-  const { access_token } = req.query;
+  try {
+    const { access_token } = req.query;
 
-  const response = await axios.get('https://api.spotify.com/v1/me/player/recently-played?limit=10', {
-    headers: {
-      Authorization: `Bearer ${access_token}`,
-    },
-  });
+    const response = await axios.get('https://api.spotify.com/v1/me/player/recently-played?limit=10', {
+      headers: {
+        Authorization: `Bearer ${access_token}`,
+      },
+    });
 
-  const songs = response.data.items.map(item => ({
-    songName: item.track.name,
-    artistName: item.track.artists[0].name,
-    artistLink: item.track.artists[0].external_urls.spotify,
-  }));
+    const songs = response.data.items.map(item => ({
+      songName: item.track.name,
+      artistName: item.track.artists[0].name,
+      artistLink: item.track.artists[0].external_urls.spotify,
+    }));
 
-  res.status(200).json(songs);
+    res.status(200).json(songs);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Internal Server Error', details: error.message });
+  }
 }
