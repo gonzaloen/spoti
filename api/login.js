12f8cd126899069c50ed7bca90efca6d080ae90d
gonzaloen <34185901+gonzaloen@users.noreply.github.com>
2024-11-09 17:14:02 +0100

    Update login.js

--- api/login.js
+++ api/login.js
@@ -1,12 +1,17 @@
 import querystring from 'querystring';
 
 export default function handler(req, res) {
-  const scope = 'user-read-recently-played';
-  const authURL = `https://accounts.spotify.com/authorize?${querystring.stringify({
-    response_type: 'code',
-    client_id: process.env.SPOTIFY_CLIENT_ID,
-    scope,
-    redirect_uri: process.env.SPOTIFY_REDIRECT_URI,
-  })}`;
-  res.redirect(authURL);
+  try {
+    const scope = 'user-read-recently-played';
+    const authURL = `https://accounts.spotify.com/authorize?${querystring.stringify({
+      response_type: 'code',
+      client_id: process.env.SPOTIFY_CLIENT_ID,
+      scope,
+      redirect_uri: process.env.SPOTIFY_REDIRECT_URI,
+    })}`;
+    res.redirect(authURL);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Internal Server Error', details: error.message });
+  }
 }
