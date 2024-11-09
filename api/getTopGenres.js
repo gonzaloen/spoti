b1a16db34bf270641f7fd6eb7e986545db3ec8a8
gonzaloen <34185901+gonzaloen@users.noreply.github.com>
2024-11-09 21:50:32 +0100

    Update getTopGenres.js

--- api/getTopGenres.js
+++ api/getTopGenres.js
@@ -6,39 +6,29 @@ export default async function handler(req, res) {
   const accessToken = cookies.access_token;
 
   if (!accessToken) {
-    console.error("Error: No hay token de acceso en la solicitud");
     return res.status(401).json({ error: 'No autenticado' });
   }
 
-  console.log("Token de acceso en la solicitud:", accessToken);
-
   try {
-    console.log("Obteniendo géneros más escuchados con token:", accessToken);
-    const response = await axios.get('https://api.spotify.com/v1/me/top/artists', {
+    const response = await axios.get('https://api.spotify.com/v1/me/following', {
       headers: {
         Authorization: `Bearer ${accessToken}`,
       },
       params: {
-        limit: 50,
+        type: 'artist',
+        limit: 10,
       },
     });
 
-    console.log("Respuesta de artistas para géneros:", response.data);
-    const genresCount = {};
-    response.data.items.forEach(artist => {
-      artist.genres.forEach(genre => {
-        genresCount[genre] = (genresCount[genre] || 0) + 1;
-      });
-    });
+    const artists = response.data.artists.items.map(artist => ({
+      name: artist.name,
+      link: artist.external_urls.spotify,
+      image: artist.images[0]?.url,
+      followers: artist.followers.total,
+    }));
 
-    const topGenres = Object.entries(genresCount)
-      .sort((a, b) => b[1] - a[1])
-      .slice(0, 5)
-      .map(([genre]) => genre);
-
-    res.status(200).json(topGenres);
+    res.status(200).json(artists);
   } catch (error) {
-    console.error('Error al obtener géneros más escuchados:', error.message);
-    res.status(500).json({ error: 'Error al obtener géneros', details: error.message });
+    res.status(500).json({ error: 'Error al obtener artistas seguidos', details: error.message });
   }
 }
