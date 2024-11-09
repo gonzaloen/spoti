71449d8a8ca55398c1547e4aec260fae7eb871f2
gonzaloen <34185901+gonzaloen@users.noreply.github.com>
2024-11-09 17:24:44 +0100

    Update login.js

--- api/login.js
+++ api/login.js
@@ -1,7 +1,18 @@
 import querystring from 'querystring';
+import cookie from 'cookie';
 
 export default function handler(req, res) {
   try {
+    // Verificar si el token de acceso ya existe en las cookies
+    const cookies = cookie.parse(req.headers.cookie || '');
+    const accessToken = cookies.access_token;
+
+    // Si el token de acceso existe, redirige al usuario a la página protegida
+    if (accessToken) {
+      return res.redirect('/api/recentlyPlayed');
+    }
+
+    // Si no hay token, redirige a la autenticación de Spotify
     const scope = 'user-read-recently-played';
     const authURL = `https://accounts.spotify.com/authorize?${querystring.stringify({
       response_type: 'code',
@@ -9,6 +20,7 @@ export default function handler(req, res) {
       scope,
       redirect_uri: process.env.SPOTIFY_REDIRECT_URI,
     })}`;
+
     res.redirect(authURL);
   } catch (error) {
     console.error(error);
