d70f6da1bae6c2f83dd3753ea704244d1d2469ff
gonzaloen <34185901+gonzaloen@users.noreply.github.com>
2024-11-09 20:54:10 +0100

    Update login.js

--- api/login.js
+++ api/login.js
@@ -1,26 +1,24 @@
-/*import querystring from 'querystring';
+/*
+
+import querystring from 'querystring';
 
 export default function handler(req, res) {
-  try {
-    const scope = 'user-read-recently-played';
-    const authURL = `https://accounts.spotify.com/authorize?${querystring.stringify({
-      response_type: 'code',
-      client_id: process.env.SPOTIFY_CLIENT_ID,
-      scope,
-      redirect_uri: process.env.SPOTIFY_REDIRECT_URI, // Esta URL debe coincidir con la configurada en Spotify Developer Dashboard
-    })}`;
-    res.redirect(authURL);
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: 'Internal Server Error', details: error.message });
-  }
+  const scope = 'user-read-recently-played';
+  const authURL = `https://accounts.spotify.com/authorize?${querystring.stringify({
+    response_type: 'code',
+    client_id: process.env.SPOTIFY_CLIENT_ID,
+    scope,
+    redirect_uri: process.env.SPOTIFY_REDIRECT_URI,
+  })}`;
+
+  res.redirect(authURL);
 }
 */
 
 import querystring from 'querystring';
 
 export default function handler(req, res) {
-  const scope = 'user-read-recently-played';
+  const scope = 'user-follow-read user-top-read';
   const authURL = `https://accounts.spotify.com/authorize?${querystring.stringify({
     response_type: 'code',
     client_id: process.env.SPOTIFY_CLIENT_ID,
