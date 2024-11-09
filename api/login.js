7e2baab2ceda041f16cfcec70863b8f844d7f98e
gonzaloen <34185901+gonzaloen@users.noreply.github.com>
2024-11-09 16:56:58 +0100

    Update login.js

--- api/login.js
+++ api/login.js
@@ -2,15 +2,11 @@ import querystring from 'querystring';
 
 export default function handler(req, res) {
   const scope = 'user-read-recently-played';
-  const redirectUri = process.env.SPOTIFY_REDIRECT_URI;
-  const clientId = process.env.SPOTIFY_CLIENT_ID;
-
   const authURL = `https://accounts.spotify.com/authorize?${querystring.stringify({
     response_type: 'code',
-    client_id: clientId,
+    client_id: process.env.SPOTIFY_CLIENT_ID,
     scope,
-    redirect_uri: redirectUri,
+    redirect_uri: process.env.SPOTIFY_REDIRECT_URI,
   })}`;
-
   res.redirect(authURL);
 }
