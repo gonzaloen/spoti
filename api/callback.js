ebdf757127850a1c0c5c69266e3e1fe8b6e99311
gonzaloen <34185901+gonzaloen@users.noreply.github.com>
2024-11-09 16:57:18 +0100

    Update callback.js

--- api/callback.js
+++ api/callback.js
@@ -8,7 +8,7 @@ export default async function handler(req, res) {
     'https://accounts.spotify.com/api/token',
     querystring.stringify({
       grant_type: 'authorization_code',
-      code: code,
+      code,
       redirect_uri: process.env.SPOTIFY_REDIRECT_URI,
       client_id: process.env.SPOTIFY_CLIENT_ID,
       client_secret: process.env.SPOTIFY_CLIENT_SECRET,
