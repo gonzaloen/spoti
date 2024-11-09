9cd8e9359fcf2e27944f5620db14cd31c0b9e415
gonzaloen <34185901+gonzaloen@users.noreply.github.com>
2024-11-09 16:40:48 +0100

    Update and rename login.js to api/callback.js

---
+++ api/callback.js
@@ -0,0 +1,21 @@
+import axios from 'axios';
+import querystring from 'querystring';
+
+export default async function handler(req, res) {
+  const { code } = req.query;
+
+  const tokenResponse = await axios.post(
+    'https://accounts.spotify.com/api/token',
+    querystring.stringify({
+      grant_type: 'authorization_code',
+      code: code,
+      redirect_uri: process.env.SPOTIFY_REDIRECT_URI,
+      client_id: process.env.SPOTIFY_CLIENT_ID,
+      client_secret: process.env.SPOTIFY_CLIENT_SECRET,
+    }),
+    { headers: { 'Content-Type': 'application/x-www-form-urlencoded' } }
+  );
+
+  const { access_token } = tokenResponse.data;
+  res.redirect(`/api/recentlyPlayed?access_token=${access_token}`);
+}
