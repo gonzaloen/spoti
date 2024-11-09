89cb8b96fffd2f649191387686aa8032aaac0e5a
gonzaloen <34185901+gonzaloen@users.noreply.github.com>
2024-11-09 17:14:17 +0100

    Update callback.js

--- api/callback.js
+++ api/callback.js
@@ -2,20 +2,25 @@ import axios from 'axios';
 import querystring from 'querystring';
 
 export default async function handler(req, res) {
-  const { code } = req.query;
+  try {
+    const { code } = req.query;
 
-  const tokenResponse = await axios.post(
-    'https://accounts.spotify.com/api/token',
-    querystring.stringify({
-      grant_type: 'authorization_code',
-      code,
-      redirect_uri: process.env.SPOTIFY_REDIRECT_URI,
-      client_id: process.env.SPOTIFY_CLIENT_ID,
-      client_secret: process.env.SPOTIFY_CLIENT_SECRET,
-    }),
-    { headers: { 'Content-Type': 'application/x-www-form-urlencoded' } }
-  );
+    const tokenResponse = await axios.post(
+      'https://accounts.spotify.com/api/token',
+      querystring.stringify({
+        grant_type: 'authorization_code',
+        code,
+        redirect_uri: process.env.SPOTIFY_REDIRECT_URI,
+        client_id: process.env.SPOTIFY_CLIENT_ID,
+        client_secret: process.env.SPOTIFY_CLIENT_SECRET,
+      }),
+      { headers: { 'Content-Type': 'application/x-www-form-urlencoded' } }
+    );
 
-  const { access_token } = tokenResponse.data;
-  res.redirect(`/api/recentlyPlayed?access_token=${access_token}`);
+    const { access_token } = tokenResponse.data;
+    res.redirect(`/api/recentlyPlayed?access_token=${access_token}`);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Internal Server Error', details: error.message });
+  }
 }
