011edcd89480af49c342b4ff06684f8a6acc592b
gonzaloen <34185901+gonzaloen@users.noreply.github.com>
2024-11-09 17:44:14 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,34 +1,44 @@
 import { useEffect, useState } from 'react';
+import cookie from 'js-cookie';
+import Router from 'next/router';
 
 export default function Home() {
   const [songs, setSongs] = useState([]);
   const [isAuthenticated, setIsAuthenticated] = useState(false);
 
   useEffect(() => {
-    async function fetchSongs() {
-      try {
-        const response = await fetch('/api/recentlyPlayed');
-        if (response.ok) {
-          const data = await response.json();
-          setSongs(data);
-          setIsAuthenticated(true);
-        } else {
-          setIsAuthenticated(false);
-        }
-      } catch (error) {
-        console.error('Error fetching songs:', error);
-      }
-    }
+    // Verifica si existe el token en las cookies
+    const accessToken = cookie.get('access_token');
 
-    fetchSongs();
+    if (accessToken) {
+      // Si hay un token, intenta obtener las canciones recientes
+      async function fetchSongs() {
+        try {
+          const response = await fetch(`/api/recentlyPlayed?access_token=${accessToken}`);
+          if (response.ok) {
+            const data = await response.json();
+            setSongs(data);
+            setIsAuthenticated(true);
+          } else {
+            setIsAuthenticated(false);
+          }
+        } catch (error) {
+          console.error('Error fetching songs:', error);
+        }
+      }
+
+      fetchSongs();
+    } else {
+      // Si no hay token, redirige al usuario a /api/login para autenticarse
+      Router.push('/api/login');
+    }
   }, []);
 
   if (!isAuthenticated) {
     return (
       <div>
         <h1>Bienvenido a Spotify Demo</h1>
-        <p>Para ver tus últimas canciones, por favor inicia sesión con Spotify.</p>
-        <a href="/api/login">Iniciar Sesión con Spotify</a>
+        <p>Redirigiendo a la página de autenticación...</p>
       </div>
     );
   }
