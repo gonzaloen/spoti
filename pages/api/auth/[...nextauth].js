3d261bb3958709b6e096f08e40f806f5933c97b4
gonzaloen <34185901+gonzaloen@users.noreply.github.com>
2024-11-10 21:06:12 +0100

    Update [...nextauth].js

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,29 +1,34 @@
-// /pages/api/auth/[...nextauth].js
-import NextAuth from 'next-auth';
-import SpotifyProvider from 'next-auth/providers/spotify';
+// Ruta: /pages/api/auth/[...nextauth].js
+
+import NextAuth from "next-auth";
+import SpotifyProvider from "next-auth/providers/spotify";
 
 export default NextAuth({
   providers: [
     SpotifyProvider({
       clientId: process.env.SPOTIFY_CLIENT_ID,
       clientSecret: process.env.SPOTIFY_CLIENT_SECRET,
-      authorization:
-        'https://accounts.spotify.com/authorize?scope=user-read-email,playlist-read-private,user-top-read,user-follow-read',
+      authorization: {
+        params: {
+          scope: "user-read-email user-follow-read user-read-recently-played user-top-read",
+        },
+      },
     }),
   ],
   secret: process.env.NEXTAUTH_SECRET,
+  pages: {
+    signIn: '/api/login', // Ruta personalizada para el login si lo necesitas
+  },
   callbacks: {
+    async session({ session, token }) {
+      session.user.accessToken = token.accessToken;
+      return session;
+    },
     async jwt({ token, account }) {
-      // If initial sign in, persist Spotify access token to the token
-      if (account?.accessToken) {
-        token.accessToken = account.accessToken;
+      if (account) {
+        token.accessToken = account.access_token;
       }
       return token;
-    },
-    async session({ session, token }) {
-      // Attach access token to session
-      session.accessToken = token.accessToken;
-      return session;
     },
   },
 });
