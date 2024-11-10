6d5b63ae7a5faaede5f72be7184b2a78dcc6d93f
gonzaloen <34185901+gonzaloen@users.noreply.github.com>
2024-11-10 18:56:14 +0100

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,7 @@
+// /pages/_app.js
+
 import '../styles/styles.css';
 
-function MyApp({ Component, pageProps }) {
+export default function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
 }
-
-export default MyApp;
